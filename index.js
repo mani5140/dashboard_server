4e0c347d19c0006c1f6f42ebf8a1d9d94c845d10
mani5140 <94881070+mani5140@users.noreply.github.com>
2023-04-20 02:46:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const pieSchema = mongoose.Schema({
+    product:String,
+    units:Number
+});
+
+const lineSchema = mongoose.Schema({
+    weeks:String,
+    guest:Number,
+    user:Number
+})
+
+const pieModel = mongoose.model("piecharts",pieSchema);
+const lineModel = mongoose.model("linecharts",lineSchema);
+
+app.get("/piechart", async(req,res) => {
+    let data = await pieModel.find();
+    res.send(data);
+})
+app.get("/linechart", async(req,res) => {
+  let data = await lineModel.find();
+  res.send(data);
+})
+
+app.post("/piechart", async (req,res) => {
+    let data = new pieModel(req.body);
+    let response = await data.save();
+    res.send(response);
+})
+
+app.post("/linechart", async (req,res) => {
+  let data = new lineModel(req.body);
+  let response = await data.save();
+  res.send(response);
+})
+
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* ADD DATA ONE TIME */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
+  })
+  .catch((error) => console.log(`${error} did not connect`));
